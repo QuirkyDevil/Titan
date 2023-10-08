24574ee943b13d06ec29b323d66993013b49b12b
Aarushi Deshmukh <141644524+aarushi-deshmukh@users.noreply.github.com>
2023-10-08 13:39:18 +0530

    -

--- codes/levels.gd
+++ codes/levels.gd
@@ -1,24 +1,20 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$"VBoxContainer/Level 1". grab_focus()
+	$VBoxContainer/level1. grab_focus()
 
 
 
-
-func _on_level_1_pressed():
+func _on_level1_pressed():
 	var level1= load("").instance()
 	get_tree().current_scene.add_child(level1)
 
-
-
-func _on_level_2_pressed():
+func _on_level2_pressed():
 	var level2= load("").instance()
 	get_tree().current_scene.add_child(level2)
 
 
-func _on_level_3_pressed():
+func _on_level3_pressed():
 	var level3= load("").instance()
 	get_tree().current_scene.add_child(level3)
--- codes/menu.gd
+++ codes/menu.gd
@@ -1,20 +1,20 @@
-extends Control
+extends MarginContainer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$CenterContainer/VBoxContainer/CenterContainer/VBoxContainer/StartButton. grab_focus()
 
 
-func _on_start_button_pressed():
-	pass
+
+func _on_StartButton_pressed():
+	pass # Replace with function body.
 
 
-func _on_options_button_pressed():
+func _on_OptionsButton_pressed():
 	var options= load("").instance()
 	get_tree().current_scene.add_child(options)
-	
 
 
-func _on_quit_button_pressed():
+
+func _on_QuitButton_pressed():
 	get_tree().quit()
